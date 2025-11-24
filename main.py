1b50a4b6fd65adf7464269600c48fff0491590a5
Alessandro LG <aless.lavagnino@gmail.com>
2025-11-24 11:20:32 -0600

    calcula parámetro de red y guarda su evolución en un .txt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from visualizer import Visualizer
 import subprocess
 import sys
 import numpy as np
+import os
 
 def main():
     config = load_config("config.yaml")
@@ -11,12 +12,17 @@ def main():
     # vis = Visualizer(sim, config)
     # vis.run()
     # Comentar si solo se quiere ejecutar (más rápido)
-    sim.run()
+    acceptance_rate, avg_dist_history = sim.run()
+    print(f"Simulación completada. Tasa de aceptación final: {acceptance_rate:.2f}")
 
     # Guarda posiciones finales
     positions_file = "positions_final.txt"
     np.savetxt(positions_file, sim.positions)
     print("Simulación completada. Posiciones guardadas en positions_final.txt")
+
+    # --- NUEVO BLOQUE PARA GUARDAR PARÁMETROS DE RED ---
+    output_dir = "parametros_red"
+    os.makedirs(output_dir, exist_ok=True)
 
     # Para nombrar la imagen final
     param_string = (
@@ -27,6 +33,18 @@ def main():
         f"σ{str(config['sigma']).replace('.', '-')}_"
         f"kT{str(config['kB_T']).replace('.', '-')}"
     )
+
+   # Define el nombre para el único archivo de salida
+    output_file = f"{output_dir}/{param_string}_avg_dist.txt"
+
+    # Convierte el diccionario {paso: promedio} a un array de 2 columnas para guardarlo
+    data_to_save = np.array(list(avg_dist_history.items()))
+
+    # Guarda el array en un único archivo de texto
+    np.savetxt(output_file, data_to_save, fmt='%.8f', header='Paso Promedio_Distancia_Minima')
+    
+    print(f"Evolución del parámetro de red guardada en '{output_file}'")
+
     subprocess.run([
         sys.executable, 
         "graficador.py", 
